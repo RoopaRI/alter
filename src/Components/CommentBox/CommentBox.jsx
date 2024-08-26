691786fa2ce47e5cfeb58a1f4ac271d5ccd2ccaa
Roopa S <roopa31113@gmail.com>
2024-08-26 11:16:52 +0530

    Integrated react-quill to get the desired comment box

--- src/Components/CommentBox/CommentBox.jsx
+++ src/Components/CommentBox/CommentBox.jsx
@@ -1,61 +1,46 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useQuill } from 'react-quilljs';
 import './CommentBox.css';
+import 'quill/dist/quill.snow.css';
 
 export default function CommentBox() {
-    const [comment, setComment] = useState('');
-    const [attachments, setAttachments] = useState([]);
-    const maxCharacters = 250;
-
-    const handleInputChange = (e) => {
-        if (e.target.value.length <= maxCharacters) {
-            setComment(e.target.value);
-        }
-    };
-
-    const handleBold = () => {
-        setComment(comment + '**bold**');
-    };
-
-    const handleItalic = () => {
-        setComment(comment + '_italic_');
-    };
-
-    const handleUnderline = () => {
-        setComment(comment + '<u>underline</u>');
-    };
-
-    const handleLink = () => {
-        const url = prompt("Enter the URL");
-        setComment(comment + `[Link](${url})`);
-    };
 
     const handleSubmit = () => {
         console.log("Comment:", comment);
-        console.log("Attachments:", attachments);
 
+        if (quill) {
+            quill.setText('');
+        }
         setComment('');
-        setAttachments([]);
     };
+
+    const modules = {
+        toolbar: {
+            container: [
+                [{ 'bold': true }, { 'italic': true }, { 'underline': true }], ['link'], ['send']  // Customizing Quill toolbar
+            ],
+            handlers: {
+                'send': handleSubmit
+            }
+        }
+    };
+
+    const { quill, quillRef } = useQuill({ modules });
+    const [comment, setComment] = useState('');
+
+    useEffect(() => {
+        if (quill) {
+            quill.on('text-change', () => {
+                setComment(quill.root.innerHTML);
+            });
+        }
+    }, [quill]);
+
 
     return (
         <div className='comment-section'>
             <div className="comment-box">
-                <textarea
-                    className='input'
-                    value={comment}
-                    onChange={handleInputChange}
-                    placeholder="Write a comment..."
-                />
-                <hr className='divider'/>
-                <div className='control-section'>
-                    <div className="controls"> 
-                        <button className='bold-btn' onClick={handleBold}>B</button>
-                        <button className='italic-btn' onClick={handleItalic}>I</button>
-                        <button className='underline-btn' onClick={handleUnderline}>U</button>
-                        <button className='link-btn' onClick={handleLink}><i class="fas fa-link"></i></button>
-                    </div>
-                    <button className='submit-btn' onClick={handleSubmit}>Send</button>
-                </div>   
+                <div ref={quillRef} />
             </div>
         </div>
     );
