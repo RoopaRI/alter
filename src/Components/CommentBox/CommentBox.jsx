abd49ccfbc30e4724a414594742080d2d304799d
Roopa S <roopa31113@gmail.com>
2024-08-26 12:03:17 +0530

    Implemented a custom toolbar to add image in the comment

--- src/Components/CommentBox/CommentBox.jsx
+++ src/Components/CommentBox/CommentBox.jsx
@@ -4,6 +4,19 @@ import './CommentBox.css';
 import 'quill/dist/quill.snow.css';
 
 export default function CommentBox() {
+    const [comment, setComment] = useState('');
+    const { quill, quillRef } = useQuill({
+        modules: {
+            toolbar: {
+                container: [
+                    [{ 'bold': true }, { 'italic': true }, { 'underline': true }],
+                    ['image'], // Added image button to toolbar
+                    ['send']  //Added send button
+                ]
+            }
+        },
+        placeholder: 'Write a comment...'
+    });
 
     const handleSubmit = () => {
         console.log("Comment:", comment);
@@ -14,28 +27,38 @@ export default function CommentBox() {
         setComment('');
     };
 
-    const modules = {
-        toolbar: {
-            container: [
-                [{ 'bold': true }, { 'italic': true }, { 'underline': true }], ['link'], ['send']  // Customizing Quill toolbar
-            ],
-            handlers: {
-                'send': handleSubmit
-            }
-        }
-    };
+    const handleImageUpload = () => {
+        if (!quill) return;
 
-    const { quill, quillRef } = useQuill({ modules });
-    const [comment, setComment] = useState('');
+        const input = document.createElement('input');
+        input.type = 'file';
+        input.accept = 'image/*';
+        input.onchange = async (event) => {
+            const file = event.target.files[0];
+            if (file) {
+                const reader = new FileReader();
+                reader.onload = () => {
+                    const range = quill.getSelection();
+                    if (range) { 
+                        quill.insertEmbed(range.index, 'image', reader.result);
+                    }
+                };
+                reader.readAsDataURL(file);
+            }
+        };
+        input.click();
+    };
 
     useEffect(() => {
         if (quill) {
+            quill.getModule('toolbar').addHandler('image', handleImageUpload);
+            quill.getModule('toolbar').addHandler('send', handleSubmit);
+
             quill.on('text-change', () => {
                 setComment(quill.root.innerHTML);
             });
         }
     }, [quill]);
-
 
     return (
         <div className='comment-section'>
